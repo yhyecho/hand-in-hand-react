1e634a986e3860c5e069fd7b945cb51d653167df
Echo <yhyecho@gmail.com>
2017-06-14 22:17:49 +0800

    add bcrypt.js to bcrypt password

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema(
@@ -10,4 +10,18 @@ const UserSchema = new Schema(
   { timestamps: true }
 );
 
+// UserSchema.pre() 前置钩子
+UserSchema.pre('save', function(next) {
+  let user = this, SALT_FACTOR = 5;
+  bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+// mongoose会自动把User首字母转小写，并在数据库中生成users collections
 module.exports = mongoose.model('User', UserSchema);
