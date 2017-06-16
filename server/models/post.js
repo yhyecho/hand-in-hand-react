bac5c9e8313685c4c2498c163e1c694a0e2632a2
Echo <yhyecho@gmail.com>
2017-06-16 11:35:24 +0800

    add server posts interface

---
+++ server/models/post.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema(
+  {
+    name: { type: String },
+    content: { type: String }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Post', PostSchema);
